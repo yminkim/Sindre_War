d205dcae9b4ceaf9bfa1376bbaaadf46cd41ad88
KIM YOUNGMIN <dudals6116@naver.com>
2020-05-17 16:58:02 +0900

    홍월 박스 기능 생성

---
+++ Box.cs
@@ -0,0 +1,34 @@
+//Box 
+        /*public void Box(int w, int h, int x, int y)
+        {
+            int s = x, d = y;
+            Console.SetCursorPosition(s, d);
+            Console.Write("┌");
+            for (int i = 0; i < w; i++)
+            {
+                Console.Write("─");
+            }
+            Console.WriteLine("┐");
+
+            d += 1;
+            for (int j = 0; j < h; j++)
+            {
+                Console.SetCursorPosition(s, d);
+                Console.Write("│");
+                for (int k = 1; k <= w * 2; k++)
+                {
+                    Console.Write(" ");
+                }
+                Console.Write("│");
+                Console.SetCursorPosition(s, d);
+                d += 1;
+            }
+            Console.SetCursorPosition(s, d);
+            Console.Write("└");
+            for (int g = 0; g < w; g++)
+            {
+                Console.Write("─");
+            }
+            Console.WriteLine("┘");
+        }
+        */
